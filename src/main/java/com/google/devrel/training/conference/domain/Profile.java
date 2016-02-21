f6ee49c52017c041f82d97cb8e43fc70c20a0e38
Дученчук Володимир <dvbgamesua@gmail.com>
2016-02-22 00:45:26 +0200

    conf search, create, register

--- src/main/java/com/google/devrel/training/conference/domain/Profile.java
+++ src/main/java/com/google/devrel/training/conference/domain/Profile.java
@@ -4,6 +4,10 @@ import com.google.devrel.training.conference.form.ProfileForm.TeeShirtSize;
 import com.googlecode.objectify.annotation.Entity;
 import com.googlecode.objectify.annotation.Id;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import com.google.common.collect.ImmutableList;
 
 
 // TODO indicate that this class is an Entity
@@ -15,6 +19,31 @@ public class Profile {
 
 	// TODO indicate that the userId is to be used in the Entity's key
 	@Id String userId;
+
+	// List of conferences the user has registered to attend
+	private List<String> conferenceKeysToAttend = new ArrayList<>(0);
+
+	public List<String> getConferenceKeysToAttend() {
+	        return ImmutableList.copyOf(conferenceKeysToAttend);
+	    }
+	    
+	    public void addToConferenceKeysToAttend(String conferenceKey) {
+	        conferenceKeysToAttend.add(conferenceKey);
+	    }
+	    
+	    /**
+	     * Remove the conferenceId from conferenceIdsToAttend.
+	     *
+	     * @param conferenceKey a websafe String representation of the Conference Key.
+	     */
+	    public void unregisterFromConference(String conferenceKey) {
+	        if (conferenceKeysToAttend.contains(conferenceKey)) {
+	            conferenceKeysToAttend.remove(conferenceKey);
+	        } else {
+	            throw new IllegalArgumentException("Invalid conferenceKey: " + conferenceKey);
+	        }
+	    }
+
     
     /**
      * Public constructor for Profile.
